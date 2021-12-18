48e5b0aab4f360683b78a6b618b71b7f545197b2
nascvi <vaiiho.nascimbene@gmail.com>
2021-12-18 05:50:03 +0100

    initial 🔧

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          All Trades Cent.
         </a>
       </header>
     </div>
